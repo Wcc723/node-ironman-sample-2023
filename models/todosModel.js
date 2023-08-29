0426289b28e60a7d6352588742adee73b28a830d
wcc723 <agito723@gmail.com>
2023-08-29 14:58:32 +0800

    MVC Todos Complete

--- models/todosModel.js
+++ models/todosModel.js
@@ -5,25 +5,43 @@ class TodoModel {
     this.todos = [
       {
         title: '這是預設資料',
+        complete: false,
         id: uuidv4(),
       },
     ];
   }
 
-  // 取得全部
   getAll() {
     return this.todos;
   }
 
-  // 新增資料
+  get(id) {
+    console.log('get', id);
+    return this.todos.find((todo) => todo.id === id);
+  }
+
   create(todo) {
-    const { title } = todo;
     const newTodo = {
-      title,
+      ...todo,
       id: uuidv4(),
     };
     this.todos.push(newTodo);
     return newTodo;
+  }
+
+  update(id, updatedFields) {
+    const todo = this.get(id);
+    if (todo) {
+      Object.assign(todo, updatedFields);
+    }
+    return todo;
+  }
+
+  delete(id) {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      return this.todos.splice(index, 1)[0];
+    }
   }
 }
 
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -5,7 +5,16 @@ const todoController = require('../controllers/todosController');
 // Get All
 router.get('/', todoController.getAllTodos);
 
+// Get one Todo
+router.get('/:id', todoController.getTodoById);
+
 // Create Todo
 router.post('/', todoController.createTodo);
+
+// Update Todo
+router.put('/:id', todoController.updateTodo);
+
+// Delete Todo
+router.delete('/:id', todoController.deleteTodo);
 
 module.exports = router;
