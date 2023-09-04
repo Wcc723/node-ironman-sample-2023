551085fb1c1cd621e483a66deb6669b3040b0d6f
wcc723 <agito723@gmail.com>
2023-09-04 10:14:52 +0800

    add env

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+
+const { SAMPLE_ENV } = process.env;
+console.log(SAMPLE_ENV);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
