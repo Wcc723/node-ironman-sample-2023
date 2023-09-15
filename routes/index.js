695690ec08e2366fe3d151b0410f5f2d5a8a9c00
wcc723 <agito723@gmail.com>
2023-09-15 16:15:29 +0800

    update code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+
+const { ENV_VARIALBES } = process.env;
+console.log('環境變數：', ENV_VARIALBES);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // #swagger.ignore = true
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', env: ENV_VARIALBES });
 });
 
 module.exports = router;
