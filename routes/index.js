4589e431b7e81895a8d99590bddfd6e6eb4adf1d
wcc723 <agito723@gmail.com>
2023-09-30 21:29:20 +0800

    加入環境變數

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+
+const { VARIALBES } = process.env;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // #swagger.ignore = true
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', VARIALBES });
 });
 
 module.exports = router;
